2f4ea757ac2379506261f607437c4c0508655bf5
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2019-10-29 13:45:44 +0630

    Homework

--- helloworld.py
+++ helloworld.py
@@ -16,3 +16,13 @@ total_tax
 total_paid = charge + total_tax
 total_paid
 15750.0
+
+#homework
+charge = 30000
+tax = 50 / 1000
+total_tax = charge * tax
+total_tax
+1500.0
+total_paid = charge + total_tax
+total_paid
+31500.0
