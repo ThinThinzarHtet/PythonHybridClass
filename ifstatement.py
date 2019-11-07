a272a865c050dade03d385acc9d66fc61b3f4cb3
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2019-11-07 12:03:46 +0630

    ifstatement.py

---
+++ ifstatement.py
@@ -0,0 +1,109 @@
+# x = int(input("Please enter an integer:"))
+
+# if x < 0:
+# 	x = 0
+# 	print('Negative changed to zero')
+# elif x == 0:
+# 	print('zero')
+# elif x == 1:
+# 	print('Single')
+# else:
+# 	print('More')
+
+
+# int - integer(Number)
+# str -string(character)
+# boolean - True , False
+# float - 0.02, 0.5, 0.1
+
+
+# x = int(input("Please enter an integer:"))
+
+# if x < 5:
+# 	print('x is smaller than 5')
+# elif x > 10:
+# 	print('x is bigger than 10')
+# elif x == 7:
+# 	print('x is equal to 7')
+# elif x > 5 and x < 10:
+# 	print('x is between 5 & 10')
+# else:
+# 	print('x is undefined')
+
+# x = int(input("Please enter your age: "))
+
+# if x > 0 and x <=18:
+# 	print('Go to school')
+# elif x >=19 and x <= 50:
+# 	print('Go to beach')
+# elif x >= 51 and x <= 75:
+# 	print('Go to monastary')
+# else:
+# 	print('go to somewhere that you like')
+
+#print("I don't like it")
+#print('I told that'" I don't like it")
+
+
+
+#measure some strings:
+# words = ['cat','window','defenestrate']
+# for w in words:
+# 	print(w, len(w))
+
+# fruits = ['apple','banana','cucumber','pineapple','orange']
+# for f in fruits:
+# 	print(f, len(f))
+
+# for i in range(5):
+# 	print(i)
+
+# for x in range(0,100):
+# 	print(x)
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# #output
+# 0 Mary
+# 1 had
+# 2 a
+# 3 little
+# 4 lamb
+
+# sum(range(4))
+# 6
+# list(range(4))
+# [0, 1, 2, 3]
+
+# fruit = ['apple', 'banana', 'cucumber', 'pineapple', 'orange']
+# fruit.append('grape')
+
+# fruit
+# ['apple', 'banana', 'cucumber', 'pineapple', 'orange', 'grape']
+# fruit.remove('grape')
+
+# fruit
+# ['apple', 'banana', 'cucumber', 'pineapple', 'orange']
+
+# fruit = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+# fruit.count('apple')
+# 2
+# fruit.count('tangerine')
+# 0
+# fruit.index('banana')
+# 3
+# fruit.index('banana',4)
+# 6
+# fruit.reverse()
+# fruit
+# ['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange']
+# fruit.append('grape')
+# fruit
+# ['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange', 'grape']
+# fruit.sort()
+# fruit
+# ['apple', 'apple', 'banana', 'banana', 'grape', 'kiwi', 'orange', 'pear']
+# fruit.pop()
+# 'pear'
