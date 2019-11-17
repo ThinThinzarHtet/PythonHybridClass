6b22650ba800626e773448c3fde0e071647f9673
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2019-11-17 11:24:34 +0630

    fibonacci

---
+++ Fibonacci/fib.py
@@ -0,0 +1,22 @@
+import fibo
+
+fibo.fib(500)
+
+f = fibo.fib
+f(100)
+
+fibo.fib(10000)
+
+from fibo import fib, fib2
+
+f = fibo.fib2(500)
+print(f)
+
+from fibo import *
+fib(500)
+
+import fibo as fi
+fi.fib(500)
+
+from fibo import fib as fibonacci
+fibonacci(500)
--- helloworld.py
+++ helloworld.py
@@ -1,28 +1,28 @@
 print("Hello World")
 
 #sample
-charge = 15000
-tax = 50/1000
-total_tax = charge * tax
-total_paid = charge + total_tax
-total_paid = ?
+# charge = 15000
+# tax = 50/1000
+# total_tax = charge * tax
+# total_paid = charge + total_tax
+# total_paid = ?
 
 #problem
-charge = 15000
-tax = 50 / 1000
-total_tax = charge * tax
-total_tax
-750.0
-total_paid = charge + total_tax
-total_paid
-15750.0
+# charge = 15000
+# tax = 50 / 1000
+# total_tax = charge * tax
+# total_tax
+# 750.0
+# total_paid = charge + total_tax
+# total_paid
+# 15750.0
 
 #homework
 charge = 30000
 tax = 50 / 1000
 total_tax = charge * tax
 total_tax
-1500.0
+#1500.0
 total_paid = charge + total_tax
 total_paid
-31500.0
+#31500.0
