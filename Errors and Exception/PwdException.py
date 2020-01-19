de87b6d75e822c054e2b903bb6ede92db58782a0
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2020-01-19 12:22:47 +0630

    pwdException

--- Errors and Exception/PwdException.py
+++ Errors and Exception/PwdException.py
@@ -24,7 +24,14 @@ class NumberException(Exception):
 	def __init__(self, number):
 		self.number = number
 
-# uppercase = text.isupper()
+uppers = 0
+upperlist = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split()
+
+lowers = 0
+lowerlist = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+
+numbers = 0
+numberslist = "1 2 3 4 5 6 7 8 9 0".split()
 try :
 	text = input("Enter your password: ")
 	if len(text) < 8:
@@ -33,8 +40,24 @@ try :
 	if len(text) > 72:
 		raise LongPwdException(len(text), 72)
 
-	if not re.findall("[A-Z]", uppercase):
-		raise UppercaseException("[A-Z]", text)
+	if not re.search(r"[A-Z]+", text):
+		raise UppercaseException()
+
+	for char in text:
+		if char in upperlist:
+			uppers += 1
+		raise UppercaseException(upperlist)
+
+	for char in text:
+		if char in lowerlist:
+			lowers += 1
+		raise LowercaseException(lowerlist)
+
+	for char in text:
+		if char in numberslist:
+			numbers += 1
+		raise NumberException(numberslist)
+
 except ShortPwdException as ex:
 	print("ShortPwdException: Your entered password was too short. Should be minimum {}".format(ex.atleast))
 
@@ -42,6 +65,12 @@ except LongPwdException as ex:
 	print("LongPwdException: Your entered password was too long. It should be maximum {}".format(ex.atleast))
 
 except UppercaseException as ex:
-	print("UppercaseException: Your enetered password should include at least one uppercase character")
+	print("UppercaseException: Your entered password should include at least one uppercase character")
+
+except LowercaseException as ex:
+	print("LowercaseException: Your entered password should include at least one lowercase character")
+
+except NumberException as ex:
+	print("NumberException: Your entered password should include at least one number")
 time.sleep(5)
 input("Press any key to exit...")
